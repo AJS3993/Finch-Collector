b980659b189c6108a1115d7e4cb6519495d290d0
Andrew Smith <ajs3993@gmail.com>
2019-12-19 10:01:08 -0600

    add a finch and redirect

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
   path('about/', views.about, name='about'),
   path('finches/', views.finches_index, name='index'),
   path('finches/<int:finch_id>/', views.finches_detail, name='detail'),
-  path('finches/create/', views.CatCreate.as_view(), name='cats_create'),
+  path('finches/create/', views.FinchCreate.as_view(), name='finches_create'),
 ]
