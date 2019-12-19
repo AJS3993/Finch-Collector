5e24afc89cc0869dbdbe793a3f2ce237c9cbdb7d
Andrew Smith <ajs3993@gmail.com>
2019-12-19 09:33:37 -0600

    first commit

---
+++ main_app/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from .models import Finch
+
+# Create your views here.
+from django.http import HttpResponse
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
+def about(request):
+  return render(request, 'about.html')
+
+def finches_index(request):
+  finches = Finch.objects.all()
+  return render(request, 'finches/index.html', { 'finches': finches })
+
+def finches_detail(request, finch_id):
+  finch = Finch.objects.get(id=finch_id)
+  return render(request, 'finches/detail.html', { 'finch': finch })
